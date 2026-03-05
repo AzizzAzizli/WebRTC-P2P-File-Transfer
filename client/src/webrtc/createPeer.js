dc895a72607dd3d1c4b99b83ef34eca3b9dfcf39
AzizzAzizli <azizliaziz025@gmail.com>
2026-03-06 02:26:01 +0400

    peerConfig updated

--- client/src/webrtc/createPeer.js
+++ client/src/webrtc/createPeer.js
@@ -1,7 +1,37 @@
 export const createPeer = () => {
-  return new RTCPeerConnection({
+  const peerConfig = {
     iceServers: [
-      { urls: 'stun:stun.l.google.com:19302' } 
-    ]
-  });
+      // STUN Servers - Free and public
+      { urls: "stun:stun.l.google.com:19302" },
+      { urls: "stun:stun1.l.google.com:19302" },
+      { urls: "stun:stun2.l.google.com:19302" },
+      { urls: "stun:stun3.l.google.com:19302" },
+      { urls: "stun:stun4.l.google.com:19302" },
+
+      // TURN Servers - For NAT traversal (fallback)
+      {
+        urls: "turn:openrelay.metered.ca:80",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:443",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:80?transport=tcp",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:443?transport=tcp",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+    ],
+    iceCandidatePoolSize: 10,
+  };
+
+  return new RTCPeerConnection(peerConfig);
 };
