747d297e9d028f408bef345ede165aa4a12b374f
AzizzAzizli <azizliaziz025@gmail.com>
2026-03-07 01:38:52 +0400

    Revert "test"
    This reverts commit d8375e9e4358fbdb7126691de537a4fe54f2ea5b.

--- client/src/webrtc/createPeer.js
+++ client/src/webrtc/createPeer.js
@@ -1,14 +1,3 @@
-// export const createPeer = () => {
-//   const peerConfig = {
-//     iceServers: [
-//       { urls: "stun:stun.l.google.com:19302" },
-//       { urls: "stun:stun1.l.google.com:19302" },
-//       { urls: "stun:stun2.l.google.com:19302" },
-//     ],
-//   };
-
-//   return new RTCPeerConnection(peerConfig);
-// };
 export const createPeer = () => {
   const peerConfig = {
     iceServers: [
@@ -17,11 +6,6 @@ export const createPeer = () => {
       { urls: "stun:stun2.l.google.com:19302" },
     ],
   };
-  const pc = new RTCPeerConnection(peerConfig);
-  
-  pc.oniceconnectionstatechange = () => {
-    console.log("ICE state:", pc.iceConnectionState);
-  };
-  
-  return pc;
+
+  return new RTCPeerConnection(peerConfig);
 };
