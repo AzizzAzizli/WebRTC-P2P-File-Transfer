769f6b7b7b6fdff0e1e0eff4ba57b04f6d28b748
AzizzAzizli <azizliaziz025@gmail.com>
2026-03-07 22:52:16 +0400

    stun server update

--- client/src/webrtc/createPeer.js
+++ client/src/webrtc/createPeer.js
@@ -4,6 +4,13 @@ export const createPeer = () => {
       { urls: "stun:stun.l.google.com:19302" },
       { urls: "stun:stun1.l.google.com:19302" },
       { urls: "stun:stun2.l.google.com:19302" },
+      { urls: "stun:stun3.l.google.com:19302" },
+      { urls: "stun:stun4.l.google.com:19302" },
+      { urls: "stun:stun.ekiga.net" },
+      { urls: "stun:stun.ideasip.com" },
+      { urls: "stun:stun.schlund.de" },
+      { urls: "stun:stun.voiparound.com" },
+      { urls: "stun:stun.voipbuster.com" },
     ],
   };
 
